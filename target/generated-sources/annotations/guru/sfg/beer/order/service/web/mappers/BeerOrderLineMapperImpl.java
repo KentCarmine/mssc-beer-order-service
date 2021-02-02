16794e18d6b7f799fa23f9f2ed3ddd655f6d878f
Kent Carmine <5168853+KentCarmine@users.noreply.github.com>
2021-02-02 15:48:16 -0600

    Fixed error in desync between BeerOrderStatusEnum and OrderStatusEnum. Deleted OrderStatusEnum

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-02-02T15:31:40-0600",
+    date = "2021-02-02T15:45:41-0600",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 12.0.1 (Oracle Corporation)"
 )
 @Component
