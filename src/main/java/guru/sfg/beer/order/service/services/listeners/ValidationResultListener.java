796d3659f66918b9654036c58fbb41fce384f5e0
Kent Carmine <5168853+KentCarmine@users.noreply.github.com>
2021-02-08 16:27:40 -0600

    Implemented listening for validation response and updating state machine based on validation response

---
+++ src/main/java/guru/sfg/beer/order/service/services/listeners/ValidationResultListener.java
@@ -0,0 +1,31 @@
+package guru.sfg.beer.order.service.services.listeners;
+
+import guru.sfg.beer.order.service.config.JMSConfig;
+import guru.sfg.beer.order.service.services.BeerOrderManager;
+import guru.sfg.brewery.model.events.ValidateOrderResult;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class ValidationResultListener {
+
+    private final BeerOrderManager beerOrderManager;
+
+    @JmsListener(destination = JMSConfig.VALIDATE_ORDER_RESPONSE_QUEUE)
+    public void listen(@Payload ValidateOrderResult validateOrderResult) {
+        final UUID beerOrderId = validateOrderResult.getOrderId();
+
+        log.debug("### Validation result for Order Id: " + beerOrderId + " is : " + validateOrderResult.getIsValid());
+
+        beerOrderManager.sendBeerOrderValidatedEvent(beerOrderId, validateOrderResult.getIsValid());
+    }
+
+}
